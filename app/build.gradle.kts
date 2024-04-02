1ce41a3b4d6c7f7a91c48434b3f5df21f19bb021
vitor <vitaoosp@gmail.com>
2024-04-02 00:24:57 -0300

    fixing packge name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.vitorthemyth.calorytracker"
+    namespace = "com.plcoding.calorytracker"
     compileSdk = ProjectConfig.compileSdk
 
     defaultConfig {
