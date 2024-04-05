edd3a66c066d25c85909e0fb46258716aa31bbbd
vitor <vitaoosp@gmail.com>
2024-04-05 01:12:11 -0300

    implementing navigation util, adding routes

--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -8,15 +8,56 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.onboarding_presentation.welcome.WelcomeScreen
+import com.plcoding.calorytracker.navigation.navigate
 import com.plcoding.calorytracker.ui.theme.CaloryTrackerTheme
+import com.vitorthemyth.core.navigation.Route
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CaloryTrackerTheme {
-                WelcomeScreen()
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Route.WELCOME
+                ) {
+                    composable(route = Route.WELCOME) {
+                        WelcomeScreen(onNavigate = navController::navigate)
+                    }
+                    composable(route = Route.AGE) {
+
+                    }
+                    composable(route = Route.GENDER) {
+
+                    }
+                    composable(route = Route.HEIGHT) {
+
+                    }
+                    composable(route = Route.WEIGHT) {
+
+                    }
+                    composable(route = Route.NUTRIENTS_GOAL) {
+
+                    }
+                    composable(route = Route.ACTIVITY) {
+
+                    }
+                    composable(route = Route.GOAL) {
+
+                    }
+                    composable(route = Route.SEARCH) {
+
+                    }
+
+                    composable(route = Route.TRACKER_OVERVIEW) {
+
+                    }
+                }
             }
         }
     }
---
+++ core/src/main/java/com/vitorthemyth/core/navigation/Route.kt
@@ -0,0 +1,15 @@
+package com.vitorthemyth.core.navigation
+
+object Route {
+    const val WELCOME = "welcome"
+    const val AGE = "age"
+    const val GENDER = "gender"
+    const val HEIGHT = "height"
+    const val WEIGHT = "weight"
+    const val NUTRIENTS_GOAL = "nutrients_goal"
+    const val ACTIVITY = "activity"
+    const val GOAL = "goal"
+
+    const val TRACKER_OVERVIEW = "tracker_overview"
+    const val SEARCH = "search"
+}
