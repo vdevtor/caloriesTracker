eb638b4397ae2b1a19182bc6d9f60b479553b536
vitor <vitaoosp@gmail.com>
2024-04-06 23:42:07 -0300

    creating the Age viewModel, adding the snackbar event to UiEvent, creting use cases packages and adding the FilterOutDigits case. finishing the age screen

--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -3,14 +3,14 @@ package com.plcoding.calorytracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.onboarding_presentation.age.AgeScreen
 import com.example.onboarding_presentation.gender.GenderScreen
 import com.example.onboarding_presentation.welcome.WelcomeScreen
 import com.plcoding.calorytracker.navigation.navigate
@@ -25,40 +25,48 @@ class MainActivity : ComponentActivity() {
         setContent {
             CaloryTrackerTheme {
                 val navController = rememberNavController()
-                NavHost(
-                    navController = navController,
-                    startDestination = Route.WELCOME
-                ) {
-                    composable(route = Route.WELCOME) {
-                        WelcomeScreen(onNavigate = navController::navigate)
-                    }
-                    composable(route = Route.AGE) {
+                val scaffoldState = rememberScaffoldState()
 
-                    }
-                    composable(route = Route.GENDER) {
-                        GenderScreen(onNavigate = navController::navigate)
-                    }
-                    composable(route = Route.HEIGHT) {
+                Scaffold(
+                    scaffoldState = scaffoldState,
+                    modifier = Modifier.fillMaxSize()
+                ) { padding ->
+                    padding.toString()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.WELCOME
+                    ) {
+                        composable(route = Route.WELCOME) {
+                            WelcomeScreen(onNavigate = navController::navigate)
+                        }
+                        composable(route = Route.AGE) {
+                            AgeScreen(onNavigate = navController::navigate,scaffoldState = scaffoldState)
+                        }
+                        composable(route = Route.GENDER) {
+                            GenderScreen(onNavigate = navController::navigate)
+                        }
+                        composable(route = Route.HEIGHT) {
 
-                    }
-                    composable(route = Route.WEIGHT) {
+                        }
+                        composable(route = Route.WEIGHT) {
 
-                    }
-                    composable(route = Route.NUTRIENTS_GOAL) {
+                        }
+                        composable(route = Route.NUTRIENTS_GOAL) {
 
-                    }
-                    composable(route = Route.ACTIVITY) {
+                        }
+                        composable(route = Route.ACTIVITY) {
 
-                    }
-                    composable(route = Route.GOAL) {
+                        }
+                        composable(route = Route.GOAL) {
 
-                    }
-                    composable(route = Route.SEARCH) {
+                        }
+                        composable(route = Route.SEARCH) {
 
-                    }
+                        }
 
-                    composable(route = Route.TRACKER_OVERVIEW) {
+                        composable(route = Route.TRACKER_OVERVIEW) {
 
+                        }
                     }
                 }
             }
--- app/src/main/java/com/plcoding/calorytracker/di/AppModule.kt
+++ app/src/main/java/com/plcoding/calorytracker/di/AppModule.kt
@@ -5,6 +5,7 @@ import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import com.vitorthemyth.core.data.preferences.DefaultPreferences
 import com.vitorthemyth.core.domain.Preferences
+import com.vitorthemyth.core.domain.use_case.FilterOutDigits
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -24,5 +25,11 @@ object AppModule {
     @Singleton
     fun providesPreferences(sharedPreferences: SharedPreferences): Preferences{
         return DefaultPreferences(sharedPreferences)
+    }
+
+    @Provides
+    @Singleton
+    fun providesFilterOutDigitsUseCases(sharedPreferences: SharedPreferences): FilterOutDigits{
+        return FilterOutDigits()
     }
 }
