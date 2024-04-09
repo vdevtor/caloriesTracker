b9e538fc5972d4a4c071a7e674428143bd6871f9
vitor <vitaoosp@gmail.com>
2024-04-09 01:45:42 -0300

    implementing the use cases for each declared fun on the repository

---
+++ tracker/tracker_domain/src/main/java/com/example/tracker_domain/use_case/GetFoodsForDate.kt
@@ -0,0 +1,14 @@
+package com.example.tracker_domain.use_case
+
+import com.example.tracker_domain.model.TrackedFood
+import com.example.tracker_domain.repository.TrackerRepository
+import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
+
+class GetFoodsForDate(
+    private val trackerRepository: TrackerRepository
+) {
+    suspend operator fun invoke(date: LocalDate): Flow<List<TrackedFood>> {
+        return trackerRepository.getFoodsForDate(date)
+    }
+}
