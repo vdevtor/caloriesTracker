f0243719b97841980b1bce4c7413ce5c86066c28
soares_vitor <soares_vitor@sicredi.com.br>
2024-04-10 15:31:07 -0300

    creating the ExpandableMeal.kt adding these composable to TrackerOverViewScreen

--- tracker/tracker_presentation/src/main/java/com/example/tracker_presentation/tracker_overview/TrackerOverViewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/example/tracker_presentation/tracker_overview/TrackerOverViewScreen.kt
@@ -6,45 +6,58 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.tracker_presentation.tracker_overview.components.DaySelector
+import com.example.tracker_presentation.tracker_overview.components.ExpandableMeal
 import com.example.tracker_presentation.tracker_overview.components.NutrientsHeader
 import com.vitorthemyth.core.util.UiEvent
 import com.vitorthemyth.core_ui.LocalSpacing
 
 @Composable
 fun TrackerOverViewScreen(
-     onNavigate: (UiEvent.Navigate) -> Unit,
-     viewModel: TrackerOverViewViewModel = hiltViewModel()
-
+   onNavigate: (UiEvent.Navigate) -> Unit,
+   viewModel: TrackerOverViewViewModel = hiltViewModel()
 ) {
-     val spacing = LocalSpacing.current
-     val state = viewModel.state
-     val context = LocalContext.current
-     
-     LazyColumn(
-          modifier = Modifier
-               .fillMaxSize()
-               .padding(bottom = spacing.spaceMedium)
-     ) {
-          item {
-               NutrientsHeader(state = state)
-               DaySelector(
-                    date = state.date,
-                    onPreviousDayClick = {
-                         viewModel.onEvent(TrackerOverViewEvent.OnPreviousDayClick)
-                    },
-                    onNextDayClick = {
-                         viewModel.onEvent(TrackerOverViewEvent.OnNextDayClick)
-                    },
-                    modifier = Modifier
-                         .fillMaxWidth()
-                         .padding(spacing.spaceMedium)
-               )
-               Spacer(modifier = Modifier.height(spacing.spaceMedium))
-          }
-     }
+   val spacing = LocalSpacing.current
+   val state = viewModel.state
+   val context = LocalContext.current
+
+   LazyColumn(
+     modifier = Modifier
+        .fillMaxSize()
+        .padding(bottom = spacing.spaceMedium)
+   ) {
+      item {
+         NutrientsHeader(state = state)
+         DaySelector(
+           date = state.date,
+           onPreviousDayClick = {
+              viewModel.onEvent(TrackerOverViewEvent.OnPreviousDayClick)
+           },
+           onNextDayClick = {
+              viewModel.onEvent(TrackerOverViewEvent.OnNextDayClick)
+           },
+           modifier = Modifier
+              .fillMaxWidth()
+              .padding(spacing.spaceMedium)
+         )
+         Spacer(modifier = Modifier.height(spacing.spaceMedium))
+      }
+      items(state.meals){meal ->
+         ExpandableMeal(
+           meal = meal,
+           content = {
+
+           },
+           onToggleClick = {
+            viewModel.onEvent(TrackerOverViewEvent.OnToggleMealClick(meal))
+           },
+           modifier = Modifier.fillMaxWidth()
+         )
+      }
+   }
 }
